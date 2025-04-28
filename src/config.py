95eb5286f0fca320eb1566fe257a9c3834b13097
kerccv <kerakk3113@gmail.com>
2025-04-28 21:15:50 +0300

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -1,10 +1,9 @@
-import os
-
-# Явное указание токена (на время теста)
 BOT_TOKEN = "7313861292:AAEThP2eynTXlRxljyas9Gma_FPvGVVZhT8"
-ADMIN_SECRET = "1293forever"
+ADMIN_PASSWORD = "1293forever"
 DATABASE_URL = "postgresql://admin:z08MOdcHDvaIkTxVqX7HiagLj3sQJk12@dpg-d07r2cp5pdvs73ae65r0-a/shop_sov1"
 
-# Проверка
-if not BOT_TOKEN:
-    raise ValueError("Токен бота не найден!")
+# Webhook settings
+WEBHOOK_URL = "https://lavandershopbot.onrender.com/webhook"
+WEBHOOK_PATH = "/webhook"
+WEB_SERVER_HOST = "0.0.0.0"
+WEB_SERVER_PORT = 8000
---
+++ webhook.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
+from src.config import BOT_TOKEN, WEBHOOK_URL, WEB_SERVER_HOST, WEB_SERVER_PORT, WEBHOOK_PATH
+from src.routers import dp
+from aiohttp import web
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=BOT_TOKEN)
+
+async def on_startup(app):
+    await bot.set_webhook(WEBHOOK_URL)
+    logger.info("Бот запущен через вебхук")
+
+app = web.Application()
+app.on_startup.append(on_startup)
+SimpleRequestHandler(dp, bot).register(app, path=WEBHOOK_PATH)
+
+if __name__ == "__main__":
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
