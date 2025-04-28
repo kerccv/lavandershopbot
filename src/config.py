d5778a78df10071644878f80afbe1ae37fd5271f
kerccv <kerakk3113@gmail.com>
2025-04-28 20:24:39 +0300

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+# Явное указание токена (на время теста)
+BOT_TOKEN = "7313861292:AAEThP2eynTXlRxljyas9Gma_FPvGVVZhT8"
+ADMIN_SECRET = "1293forever"
+DATABASE_URL = "postgresql://admin:z08MOdcHDvaIkTxVqX7HiagLj3sQJk12@dpg-d07r2cp5pdvs73ae65r0-a/shop_sov1"
 
-BOT_TOKEN = os.getenv("7313861292:AAEThP2eynTXlRxljyas9Gma_FPvGVVZhT8")
-ADMIN_SECRET = os.getenv("1293forever")  # Секретный код для входа в админку
-DATABASE_URL = "sqlite:///shop.db"        # Путь к SQLite (можно заменить на PostgreSQL)
+# Проверка
+if not BOT_TOKEN:
+    raise ValueError("Токен бота не найден!")
